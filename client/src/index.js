2ddb402db1708947004386eb29ff9dbfaca6d32c
Salah <salahelabd000@gmail.com>
2026-06-22 15:42:45 +0300

    add PWA support

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import { StoreProvider } from "./context/StoreContext"; // ← add this
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -18,3 +19,5 @@ root.render(
     </CartProvider>
   </React.StrictMode>,
 );
+
+serviceWorkerRegistration.register();
