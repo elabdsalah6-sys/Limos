467c7bf119c13af3bc6d4bc78ee14de36d1990d7
Salah <salahelabd000@gmail.com>
2026-08-09 20:35:15 +0300

    debug: add error logger, fix double providers, unregister SW

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,13 @@
-// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { CartProvider } from "./context/CartContext";
-import { StoreProvider } from "./context/StoreContext"; // ← add this
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <StoreProvider>
-        {" "}
-        {/* ← wrap App with it */}
-        <App />
-      </StoreProvider>
-    </CartProvider>
+    <App />
   </React.StrictMode>,
 );
 
